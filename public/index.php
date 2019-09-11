ad339f8f042c4e0b355d2d1c0c5d2b847f3d2a93
kimdonghyun <dhkiim7463@naver.com>
2019-09-11 14:33:07 +0900

    op01

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$layout = file_get_contents("../thema/index.html");
+
+$filename = $_SERVER['REQUEST_URI'].".html";
+
+if(file_exists("../".$filename)){
+    $body = file_get_contents("../".$filename);
+    $layout = str_replace("{{main}}",$body, $layout);
+}else{
+    $body = file_get_contents("../home.html");
+    $layout = str_replace("{{main}}",$body, $layout);
+}
+
+echo $layout;
